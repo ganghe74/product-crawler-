7d8ccbaf419377366784f756dc99f24bcf1e030e
ganghe74 <ganghe74@kookmin.ac.kr>
2019-02-17 04:37:11 +0900

    a little change to run server on online

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=xsr4&te#()78(n5ufc&52y%a@9oh39oxk1$!%!dk9_vvug+cf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
